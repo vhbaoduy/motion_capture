1b4bafde07502a17fa7655c729b2f08ad38202cf
Đinh Ngọc Ân <101492362+andythetechnerd03@users.noreply.github.com>
2023-08-22 01:46:48 +0700

    make ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,106 @@
-import CloudinaryUploadWidget from "./CloudinaryUploadWidget";
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import React, { Component } from 'react';
+import Wrapper from './components/Wrapper';
+import NavBar from './components/NavBar';
 
-import "./styles.css";
+class App extends Component {
 
-export default function App() {
-  return (
-    <div className="App">
-      <h3>Cloudinary Upload Widget Example</h3>
-      <CloudinaryUploadWidget />
-      <p>
-        <a
-          href="https://cloudinary.com/documentation/upload_widget"
-          target="_blank"
-        >
-          Upload Widget User Guide
-        </a>
-      </p>
-      <p>
-        <a
-          href="https://cloudinary.com/documentation/upload_widget_reference"
-          target="_blank"
-        >
-          Upload Widget Reference
-        </a>
-      </p>
-      <img id="uploadedimage" src=""></img>
-    </div>
-  );
+	state = {
+
+		// Initially, no file is selected
+		selectedFile: null
+	};
+
+	// On file select (from the pop up)
+	onFileChange = event => {
+
+		// Update the state
+		this.setState({ selectedFile: event.target.files[0] });
+
+	};
+
+	// On file upload (click the upload button)
+	onFileUpload = () => {
+
+		// Create an object of formData
+		const formData = new FormData();
+
+		// Update the formData object
+		formData.append(
+			"myFile",
+			this.state.selectedFile,
+			this.state.selectedFile.name
+		);
+
+		// Details of the uploaded file
+		console.log(this.state.selectedFile);
+
+		// Request made to the backend api
+		// Send formData object
+		axios.post("api/uploadfile", formData);
+	};
+
+	// File content to be displayed after
+	// file upload is complete
+	fileData = () => {
+
+		if (this.state.selectedFile) {
+
+			return (
+				<div>
+					<h2>File Details:</h2>
+					<p>File Name: {this.state.selectedFile.name}</p>
+
+					<p>File Type: {this.state.selectedFile.type}</p>
+
+					<p>
+						Last Modified:{" "}
+						{this.state.selectedFile.lastModifiedDate.toDateString()}
+					</p>
+
+				</div>
+			);
+		}
+	};
+
+
+
+	render() {
+
+    const handleUploadButtonClick = () => {
+      document.getElementById('fileInput').click();
+    };
+
+		return (
+      <div>
+      <NavBar></NavBar>
+      <Wrapper>
+      <div>
+      <h1 id="heading">
+        3D Pose Estimation
+      </h1>
+      <h2 id="heading-2">
+        Upload an image to get started!
+      </h2>
+      <div>
+        <Button variant="contained" onClick={handleUploadButtonClick}>
+          Upload!
+        </Button>{' '}
+        <input type="file"
+                id="fileInput"
+                style={{ display: 'none' }}
+                onChange={this.onFileChange}>
+
+        </input>
+        
+        </div>
+          {this.fileData()}
+        </div>
+      </Wrapper>
+      </div>
+		);
+	}
 }
+
+export default App;
