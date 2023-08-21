4c2124745111f86b14630c776f61fb9954bb8f11
ngtuthanhan <ngtuthanhan@gmail.com>
2023-08-22 01:36:02 +0700

    update

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 @app.post("/upload/")
-async def upload_files(files: List[UploadFile]):
+async def upload_files(files):
     output_files = []
 
     for uploaded_file in files:
@@ -23,11 +23,8 @@ async def upload_files(files: List[UploadFile]):
         with open(file_path, "wb") as file:
             shutil.copyfileobj(uploaded_file.file, file)
 
-        # Process the uploaded file using the AI module (replace with your actual AI module code)
-        # output_file = ai_module.process(file_path)  # Replace 'ai_module' with your AI module
-        # output_files.append(output_file)
-
     return {"message": "Files uploaded and processed successfully", "output_files": output_files}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, log_level="debug", reload=True,)
