308b943118eecce1d2f88164135244ba65bf83b9
daron <v.hbaoduy@gmail.com>
2023-08-24 00:42:27 +0700

    call api and show output

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import Wrapper from './components/Wrapper';
 import NavBar from './components/NavBar';
 import ReactPlayer from 'react-player';
-import myVideo from './demo/demo.mp4';
+// import myVideo from './demo/demo.mp4';
+import axios from "axios";
+import { getNativeSelectUtilityClasses } from '@mui/material';
 
 
 class App extends Component {
@@ -11,13 +13,15 @@ class App extends Component {
 	state = {
 
 		// Initially, no file is selected
-		selectedFile: null
+		selectedFile: null,
+    outputFile: null,
 	};
 
 	// On file select (from the pop up)
-	onFileChange = event => {
+	onFileChange = (event) => {
 
 		// Update the state
+    // console.log(event.target.files[0])
 		this.setState({ selectedFile: event.target.files[0] });
 
 	};
@@ -25,14 +29,14 @@ class App extends Component {
 
 	// File content to be displayed after
 	// file upload is complete
-	fileData = () => {
+	fileData = (file) => {
 
-		if (this.state.selectedFile) {
+		if (file) {
 
 			return (
         <div class="video-wrapper">
 					<ReactPlayer
-            url= {myVideo}
+            url= {URL.createObjectURL(file)}
             width="640px"
             height="360px"
             controls={true}
@@ -70,11 +74,33 @@ class App extends Component {
       document.getElementById('fileInput').click();
     };
 
-    const handleConvertButtonClick = () => {
+    const handleConvertButtonClick = async () => {
       if (this.state.selectedFile == undefined) {
         alert("Please upload a file first!");
       }
       else {
+          const formData = new FormData()
+          console.log(this.state.selectedFile)
+          await formData.append("video_file", this.state.selectedFile)
+          console.log(formData)
+          await axios({
+            url:'54.70.121.78:8000/video',
+            method: "POST",
+            data:formData,
+            headers:{
+              'Content-Type': 'multipart/form-data'
+            },
+            responseType: 'blob',
+          }).then(async (res) => {
+            console.log(res)
+            // const blob = await res.blob()
+            // const dataBlob = new Blob([res.data], {type:"video/mp4"})
+            // const blobURL = URL.createObjectURL(dataBlob)
+            // console.log(dataBlob)
+            // console.log(blobURL)
+            this.setState({ outputFile: res.data });
+          })
+          // callApi(this.state.selectedFile)
 
       }
     };
@@ -106,7 +132,7 @@ class App extends Component {
         </Button>{' '}
         
         </div>
-          {this.fileData()}
+          {this.fileData(this.state.selectedFile)}
         </div>
       </Wrapper>
 
@@ -115,7 +141,7 @@ class App extends Component {
       <h1 id="heading">
         Output
       </h1>
-          {this.fileData()}
+          {this.fileData(this.state.outputFile)}
         <div>
         <Button variant="contained" color='info'>Download</Button>{' '}
         </div>
